7b570177fb744092326edc44e033179db980fc95
Stephen Touset <stephen@squareup.com>
2018-01-05 21:32:28 -0800

    Remove sockets on Ctrl-C!

--- sudo_pair/src/socket.rs
+++ sudo_pair/src/socket.rs
@@ -6,7 +6,7 @@ use std::os::unix::ffi::OsStrExt;
 use std::os::unix::fs::FileTypeExt;
 use std::path::Path;
 
-use libc::{self, c_int, gid_t, mode_t, sighandler_t, uid_t};
+use libc::{self, gid_t, mode_t, uid_t};
 
 use unix_socket::{SocketAddr, UnixListener, UnixStream};
 
@@ -21,19 +21,19 @@ impl Socket {
         uid:    uid_t,
         gid:    gid_t,
         mode:   mode_t,
-        ctrl_c: sighandler_t,
     ) -> io::Result<Self> {
+        let cpath = CString::new(
+            path.as_ref().as_os_str().as_bytes()
+        )?;
+
         // if the path already exists as a socket, make a best-effort
         // attempt at unlinking it
         Self::unlink_socket(&path)?;
 
-        // accept() on the socket will block until someone connects on
-        // the other side of the socket
+        // create a socket and wait for someone to connect; we *don't*
+        // suffix this with an immediate `?` so that we have a chance
+        // to clean up after ourselves first
         let connection = UnixListener::bind(&path).and_then(|sock| {
-            let cpath = CString::new(
-                path.as_ref().as_os_str().as_bytes()
-            )?;
-
             unsafe {
                 if libc::chown(cpath.as_ptr(), uid, gid) == -1 {
                     return Err(io::Error::last_os_error());
@@ -43,19 +43,30 @@ impl Socket {
                     return Err(io::Error::last_os_error());
                 }
 
-                // temporarily install the provided SIGINT handler while we
-                // block on accept()
-                let sigint = signal(libc::SIGINT, ctrl_c)?;
-                let result = sock.accept();
-                let _      = signal(libc::SIGINT, sigint)?;
-
-                result
+                // accept() onwill block until someone connects on the
+                // other side of the socket, so we ensure that the
+                // signal handler for Ctrl-C aborts syscalls instead of
+                // restarting them automatically
+                ctrl_c_aborts_syscalls(|| {
+                    sock.accept()
+                })?
             }
-        })?;
+        });
 
-        // once the connection has been made, we don't need the socket
-        // to remain on the filesystem
-        Self::unlink_socket(&path)?;
+        // once the connection has been made (or aborted due to ctrl-c),
+        // we don't need the socket to remain on the filesystem
+        //
+        // we ignore the result of this operation (instead of returning
+        // the error because a) any error from the previous operation is
+        // of higher importance (we didn't return the error immediately
+        // because we want to unlink the socket regardless) and b) it's
+        // more important to continue the sudo session than to worry
+        // about filesystem janitorial work
+        let _ = Self::unlink_socket(&path);
+
+        // unwrap the connection and return any errors now that we've
+        // cleaned up after ourself
+        let connection = connection?;
 
         Ok(Self {
             socket: connection.0,
@@ -67,7 +78,6 @@ impl Socket {
         self.socket.shutdown(Shutdown::Both)
     }
 
-    // TODO: unlink when sudo_pair is ctrl-c'd
     fn unlink_socket<P: AsRef<Path>>(path: P) -> io::Result<()> {
         match fs::metadata(&path).map(|md| md.file_type().is_socket()) {
             // file exists, is a socket; delete it
@@ -110,12 +120,48 @@ impl Write for Socket {
     }
 }
 
-unsafe fn signal(
-    signum:  c_int,
-    handler: sighandler_t,
-) -> io::Result<sighandler_t> {
-    match libc::signal(signum, handler) {
-        libc::SIG_ERR => Err(io::Error::last_os_error()),
-        previous      => Ok(previous),
+/// Sets up a handler for Ctrl-C (SIGINT) that's a no-op, but with the
+/// SA_RESTART flag disabled, for the duration of the passed function
+/// call.
+///
+/// Disabling SA_RESTART ensures that blocking calls like `accept(2)`
+/// will be terminated upon receipt on the signal instead of
+/// automatically resuming.
+unsafe fn ctrl_c_aborts_syscalls<F, T>(func: F) -> io::Result<T>
+    where F: FnOnce() -> T
+{
+    let mut sigaction_old  = ::std::mem::uninitialized();
+    let     sigaction_null = ::std::ptr::null_mut();
+
+    // retrieve the existing handler
+    sigaction(libc::SIGINT, sigaction_null, &mut sigaction_old)?;
+
+    // copy the old handler, but mask out SA_RESTART
+    let mut sigaction_new = sigaction_old;
+    sigaction_new.sa_flags &= !libc::SA_RESTART;
+
+    // install the new handler
+    sigaction(libc::SIGINT, &sigaction_new, sigaction_null)?;
+
+    let result = func();
+
+    // reinstall the old handler
+    sigaction(libc::SIGINT, &sigaction_old, sigaction_null)?;
+
+    Ok(result)
+}
+
+/// Installs the new handler for the signal identified by `sig` if `new`
+/// is non-null. Returns the preexisting handler for the signal if `old`
+/// is non-null.
+unsafe fn sigaction(
+    sig: libc::c_int,
+    new: *const libc::sigaction,
+    old: *mut   libc::sigaction,
+) -> io::Result<()> {
+    if libc::sigaction(sig, new, old) == -1 {
+        return Err(io::Error::last_os_error())
     }
+
+    Ok(())
 }
