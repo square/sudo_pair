f7381bdd3215c880912ea4c3ca4ae0f012ac3d07
Stephen Touset <stephen@squareup.com>
2018-04-21 14:15:02 -0700

    Temporarily log errors from print_info

--- sudo_pair/src/lib.rs
+++ sudo_pair/src/lib.rs
@@ -138,9 +138,11 @@ impl SudoPair {
             &self.settings.user_prompt_path
         ).unwrap_or_else(|_| DEFAULT_USER_PROMPT.to_owned());
 
+        // TODO: this is returning an error even though it printf, I'm not
+        // entirely sure why
         let _ = self.plugin.print_info(
             self.template_expand(&template[..])
-        )?;
+        );
 
         Ok(())
     }
@@ -168,6 +170,8 @@ impl SudoPair {
             &self.settings.pair_prompt_path
         ).unwrap_or_else(|_| DEFAULT_PAIR_PROMPT.to_owned());
 
+        let prompt = self.template_expand(&template[..]);
+
         let socket = self.socket
             .as_mut()
             .ok_or_else(|| ErrorKind::Unauthorized("unable to connect to a pair".into()))?;
@@ -176,7 +180,7 @@ impl SudoPair {
             ::std::mem::uninitialized()
         };
 
-        let _ = socket.write(&template[..])
+        let _ = socket.write(&prompt[..])
             .chain_err(|| ErrorKind::Unauthorized("unable to ask pair for approval".into()))?;
 
         // read exactly one byte back from the socket for the
