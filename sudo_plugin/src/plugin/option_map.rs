65f4f8acf01c66ff6c08e2f7b4037f004a211718
Stephen Touset <stephen@squareup.com>
2018-04-25 12:58:24 -0400

    OptionMap can't return an error, so remove the unnecessary Result wrapper

--- sudo_plugin/src/plugin/option_map.rs
+++ sudo_plugin/src/plugin/option_map.rs
@@ -37,28 +37,42 @@ pub struct ParseListError();
 
 impl OptionMap {
     /// Initializes the `OptionMap` from a pointer to the options
-    /// provided when `sudo` invokes the plugin's entry function.
+    /// provided when `sudo` invokes the plugin's entry function. The
+    /// format of these is a NUL-terminated array of NUL-terminated
+    /// strings in "key=value" format with the array terminated by a
+    /// NULL pointer.
     ///
-    /// The format of these is a NUL-terminated array of NUL-terminated
-    /// strings in "key=value" format.
-    pub unsafe fn new(mut ptr: *const *const c_char) -> Result<Self> {
-        let mut map = HashMap::new();
-
+    /// This method cannot be safe, since it relies on the caller to
+    /// place a NULL byte as the final array entry. In the absence of
+    /// such a NULL byte, there is no other way to detect the end of
+    /// the options list.
+    pub unsafe fn new(mut ptr: *const *const c_char) -> Self {
         // if the pointer is null, we weren't given a list of settings,
         // so go ahead and return the empty map
         if ptr.is_null() {
-            return Ok(OptionMap(map));
+            return Self::default();
         }
 
+        let mut map = HashMap::new();
+
+        // iterate through each pointer in the array until encountering
+        // a NULL (which terminates the array)
         while !(*ptr).is_null() {
             let bytes = CStr::from_ptr(*ptr).to_bytes();
             let sep = bytes.iter().position(|b| *b == OPTIONS_SEPARATOR);
 
+            // separators might not exist (e.g., in the case of parsing
+            // plugin options; for this case, we use the full entry as
+            // both the name of the key and its value
             let (k, v) = match sep {
                 Some(s) => { ( &bytes[..s], &bytes[s+1..] ) }
                 None    => { ( &bytes[..],  &bytes[..] ) }
             };
 
+            // the return value is ignored because there's not an
+            // otherwise-reasonable way to handle duplicate key names;
+            // that said, the implications of this are that the last
+            // value of a given key is the one that's set
             let _ = map.insert(
                 k.to_owned(),
                 v.to_owned(),
@@ -67,7 +81,7 @@ impl OptionMap {
             ptr = ptr.offset(1);
         }
 
-        Ok(OptionMap(map))
+        OptionMap(map)
     }
 
     /// Gets the value of a key as a string. Returns `None` if no such
@@ -97,6 +111,12 @@ impl OptionMap {
     /// value and/or key are not guaranteed to be UTF-8 strings.
     pub fn get_raw(&self, k: &[u8]) -> Option<&[u8]> {
         self.0.get(k).map(Vec::as_slice)
+    }
+}
+
+impl Default for OptionMap {
+    fn default() -> OptionMap {
+        OptionMap(HashMap::new())
     }
 }
 
