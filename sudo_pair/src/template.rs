518d083f6bc3102520173cda89fb7d3c90b259db
Stephen Touset <stephen@squareup.com>
2018-05-02 12:51:55 -0700

    Test templating language

--- sudo_pair/src/template.rs
+++ sudo_pair/src/template.rs
@@ -17,11 +17,11 @@ use std::io::{Read, Result};
 use std::fs::File;
 use std::path::Path;
 
-const DEFAULT_ESCAPE : u8 = b'%';
+const DEFAULT_ESCAPE_BYTE : u8 = b'%';
 
 pub(crate) struct Template {
-    template: Vec<u8>,
-    escape:   u8,
+    raw:    Vec<u8>,
+    escape: u8,
 }
 
 impl Template {
@@ -37,8 +37,8 @@ impl Template {
     }
 
     pub(crate) fn expand(&self, spec: &HashMap<u8, Vec<u8>>) -> Vec<u8> {
-        let mut result = Vec::with_capacity(self.template.len());
-        let mut iter   = self.template.iter().cloned();
+        let mut result = Vec::with_capacity(self.raw.len());
+        let mut iter   = self.raw.iter().cloned();
 
         while iter.len() != 0 {
             // copy literally everything up to the next escape character
@@ -77,8 +77,8 @@ impl Template {
 impl Default for Template {
     fn default() -> Self {
         Self {
-            template: Vec::new(),
-            escape:   DEFAULT_ESCAPE,
+            raw:    Vec::new(),
+            escape: DEFAULT_ESCAPE_BYTE,
         }
     }
 }
@@ -91,6 +91,177 @@ impl<'a> From<&'a [u8]> for Template {
 
 impl From<Vec<u8>> for Template {
     fn from(buf: Vec<u8>) -> Self {
-        Self { template: buf, .. Self::default() }
+        Self { raw: buf, .. Self::default() }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn new_template_from_buffer() {
+        let raw      = b"simple template";
+        let template = Template::from(&raw[..]);
+
+        assert_eq!(raw[..], template.raw[..]);
+    }
+
+    #[test]
+    fn new_template_default_escape_character() {
+        let template = Template::from(&b""[..]);
+
+        assert_eq!(DEFAULT_ESCAPE_BYTE, template.escape);
+    }
+
+    #[test]
+    fn no_expansions() {
+        let raw      = b"this has no expansions";
+        let template = Template::from(&raw[..]);
+        let spec     = HashMap::new();
+
+        assert_eq!(raw[..], template.expand(&spec)[..]);
+    }
+
+    #[test]
+    fn expansions() {
+        let     raw      = b"a: %a, b: %b";
+        let     template = Template::from(&raw[..]);
+        let mut spec     = HashMap::new();
+
+        let _ = spec.insert(b'a', b"foo".to_vec());
+        let _ = spec.insert(b'b', b"bar".to_vec());
+
+        assert_eq!(
+            b"a: foo, b: bar"[..],
+            template.expand(&spec)[..],
+        );
+    }
+
+    #[test]
+    fn repeated_expansions() {
+        let     raw      = b"%a%a%a%b%a%a%b";
+        let     template = Template::from(&raw[..]);
+        let mut spec     = HashMap::new();
+
+        let _ = spec.insert(b'a', b"x".to_vec());
+        let _ = spec.insert(b'b', b"y".to_vec());
+
+        assert_eq!(
+            b"xxxyxxy"[..],
+            template.expand(&spec)[..],
+        );
+    }
+
+    #[test]
+    fn expansion_inserts_itself() {
+        let     raw      = b"test %x test";
+        let     template = Template::from(&raw[..]);
+        let mut spec     = HashMap::new();
+
+        let _ = spec.insert(b'x', b"x".to_vec());
+
+        assert_eq!(
+            b"test x test"[..],
+            template.expand(&spec)[..],
+        );
+    }
+
+    #[test]
+    fn expansion_isnt_recursive() {
+        let     raw      = b"test %x test";
+        let     template = Template::from(&raw[..]);
+        let mut spec     = HashMap::new();
+
+        let _ = spec.insert(b'x', b"%x %y %z % %%".to_vec());
+        let _ = spec.insert(b'y', b"BUG".to_vec());
+
+        assert_eq!(
+            b"test %x %y %z % %% test"[..],
+            template.expand(&spec)[..],
+        );
+    }
+
+    #[test]
+    fn expansion_inserts_nothing() {
+        let     raw      = b"test %X test";
+        let     template = Template::from(&raw[..]);
+        let mut spec     = HashMap::new();
+
+        let _ = spec.insert(b'X', b"".to_vec());
+
+        assert_eq!(
+            b"test  test"[..],
+            template.expand(&spec)[..],
+        );
+    }
+
+    #[test]
+    fn unused_expansions() {
+        let     raw      = b"only y should be expanded %y";
+        let     template = Template::from(&raw[..]);
+        let mut spec     = HashMap::new();
+
+        let _ = spec.insert(b'y', b"qwerty".to_vec());
+        let _ = spec.insert(b'n', b"uiop[".to_vec());
+
+        assert_eq!(
+            b"only y should be expanded qwerty"[..],
+            template.expand(&spec)[..],
+        );
+    }
+
+    #[test]
+    fn literals() {
+        let     raw      = b"a: %a, b: %b";
+        let     template = Template::from(&raw[..]);
+        let mut spec     = HashMap::new();
+
+        let _ = spec.insert(b'b', b"bar".to_vec());
+
+        assert_eq!(
+            b"a: a, b: bar"[..],
+            template.expand(&spec)[..],
+        );
+    }
+
+    #[test]
+    fn literal_escape_character() {
+        let raw      = b"%%%%%%%%%%%%%%";
+        let template = Template::from(&raw[..]);
+        let spec     = HashMap::new();
+
+        assert_eq!(
+            b"%%%%%%%"[..],
+            template.expand(&spec)[..],
+        );
+    }
+
+    // you can currently provide an expansion for the escape character,
+    // which prevents ever being able to insert an escape character
+    // literal; this isn't worth fixing
+    #[test]
+    fn bug_wontfix_expand_escape_character() {
+        let     raw      = b"|%%|";
+        let     template = Template::from(&raw[..]);
+        let mut spec     = HashMap::new();
+
+        let _ = spec.insert(b'%', b"x".to_vec());
+
+        assert_eq!(b"|x|"[..], template.expand(&spec)[..]);
+    }
+
+    // `take_while` silently eats one extra character off of the `Iter`,
+    // since it needs to call `Iter::next` before it can check the value,
+    // and this leads us to not being able to detect the difference
+    // between a normal EOF and an EOF immediately after a lone escape
+    // character; this isn't worth fixing
+    #[test]
+    fn bug_wontfix_swallow_trailing_escape_character() {
+        let raw      = b"some text%";
+        let template = Template::from(&raw[..]);
+        let spec     = HashMap::new();
+
+        assert_eq!(b"some text"[..], template.expand(&spec)[..]);
     }
 }
